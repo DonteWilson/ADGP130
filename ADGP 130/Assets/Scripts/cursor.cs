73718b66b6b86fc5927ff65868d9a70eec78d2a3
RvaughnAIEYear1 <Rvaughn2525@gmail.com>
2016-02-16 14:13:49 -0600

    level prefab

--- ADGP 130/Assets/Scripts/cursor.cs
+++ ADGP 130/Assets/Scripts/cursor.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class cursor : MonoBehaviour {
+public class cursor : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition, 10);
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, +10));
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            GetComponent<AudioSource>().Play();
+
+            if (hit != null && hit.collider != null)
+            {
+                Destroy(hit.collider.gameObject);
+                //hit.collider.GetComponent<Health>().hp -= 1;
+                Debug.Log("hit");
+
+            }
+        }
+
+
+    }
 }
