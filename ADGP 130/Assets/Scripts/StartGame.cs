efa4e4f9b3210ec8954602f4234c64c5a908d6ac
dwilsonaieyr1 <JittNA@aol.com>
2016-02-16 15:04:19 -0600

    Level 2

--- ADGP 130/Assets/Scripts/StartGame.cs
+++ ADGP 130/Assets/Scripts/StartGame.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class StartGame : MonoBehaviour {
+public class StartGame : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void OnGUI()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        //if retry button is pressed load scene 0 the game
+        GUI.contentColor = Color.white;
+        if (GUI.Button(new Rect(Screen.width / 7, Screen.height / 2 - 10, 300, 100), "Start"))
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            Application.LoadLevel(1);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+        //and quit button
+        if (GUI.Button(new Rect(Screen.width / 7, Screen.height / 2 + 110, 300, 100), "Quit"))
+        {
+            Application.Quit();
+        }
+    }
+
 }
