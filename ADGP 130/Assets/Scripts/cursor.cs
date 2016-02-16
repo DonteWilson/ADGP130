4d1037ab0a2a23b9147c03ec50e70c10527c4d65
dwilsonaieyr1 <JittNA@aol.com>
2016-02-16 15:19:45 -0600

    matching rorys

--- ADGP 130/Assets/Scripts/cursor.cs
+++ ADGP 130/Assets/Scripts/cursor.cs
@@ -20,29 +20,6 @@ public class cursor : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition, 10);
-
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x - 8, Input.mousePosition.y + 11, +10));
-        if (Input.GetMouseButtonDown(0) == true)
-        {
-            GetComponent<AudioSource>().Play();
-
-            if (hit != null && hit.collider != null)
-            {
-                Destroy(hit.collider.gameObject);
-                //hit.collider.GetComponent<Health>().hp -= 1;
-                Debug.Log("hit");
-
-            }
-        }
-
-
-    }
-}
-
     {                
         transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, +10));
     }
