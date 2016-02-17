78d7c2a86d6252032367183984eeeb66eb81a0cd
dwilsonaieyr1 <JittNA@aol.com>
2016-02-16 18:01:49 -0600

    move

--- ADGP 130/Assets/Scripts/cursor.cs
+++ ADGP 130/Assets/Scripts/cursor.cs
@@ -20,17 +20,25 @@ public class Cursor : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {                
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition, 10);
+
         transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, +10));
         if (Input.GetMouseButtonDown(0) == true)
         {
             GetComponent<AudioSource>().Play();
+            if (hit != null && hit.collider != null)
+            {
+                Destroy(hit.collider.gameObject);
+                //hit.collider.GetComponent<Health>().hp -= 1;
+            }
+
         }
     }
 
     public void Shoot()
     {
-        Debug.Log("do it");
+       Debug.Log("do it");
     }
 }
 
