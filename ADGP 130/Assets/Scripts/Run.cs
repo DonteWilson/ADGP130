b5913c9f6eef89867c9e99484847c07fc30ccf8e
dwilsonaieyr1 <JittNA@aol.com>
2016-02-22 17:54:49 -0600

    commit
    commit

--- ADGP 130/Assets/Scripts/Run.cs
+++ ADGP 130/Assets/Scripts/Run.cs
@@ -1,44 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class Run : MonoBehaviour
 {
-
     public Vector2 begin;
     public Vector2 end;
     public float time_for_move;
     Animator anim;
     int stop = Animator.StringToHash("stand");
-
     float timer;
-
     // Use this for initialization
     void Start()
     {
-
         transform.position = begin;
         timer = time_for_move;
         anim = GetComponent<Animator>();
-
     }
-
     // Update is called once per frame
     void Update()
     {
-
         timer -= Time.deltaTime;
-
         if (timer > 0)
         {
-            
-
+            anim.SetBool("IsWalking", true);
             Vector2 distance = end - begin;
             float degree_of_movement = (time_for_move - timer) / time_for_move;
             transform.position = new Vector2(begin.x + (distance.x * degree_of_movement), begin.y + (distance.y * degree_of_movement));
         }
-        if (time_for_move == 0)
+        if (time_for_move <= 1)
         {
-            anim.SetTrigger(stop);
+            gameObject.GetComponent<Animator>().SetBool("IsWalking", false);
+
         }
     }
 }
--- ADGP 130/Assets/Scripts/cursor.cs
+++ ADGP 130/Assets/Scripts/cursor.cs
@@ -1,30 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-
 public class Cursor : MonoBehaviour
-
-   
-
-
 
 {
     //Run Death;
-
-
-
     // Use this for initialization
     void Start()
     {
-
     }
-
-
     // Update is called once per frame
     void Update()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Input.mousePosition, 10);
-
         transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, +10));
         if (Input.GetMouseButtonDown(0) == true)
         {
@@ -32,16 +19,14 @@ public class Cursor : MonoBehaviour
             if (hit != null && hit.collider != null)
             {
                 //gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
-                Destroy(hit.collider.gameObject);
+                hit.collider.gameObject.GetComponent<Animator>().SetBool("IsDead", true);
+                //hit.collider.gameObject.transform.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
                 //hit.collider.GetComponent<Health>().hp -= 1;
             }
-
         }
     }
-
     public void Shoot()
     {
-       Debug.Log("do it");
+        Debug.Log("do it");
     }
 }
-
