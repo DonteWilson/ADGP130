92cb079d6f17c90eea1757f35e10dce291648b2e
dwilsonaieyr1 <JittNA@aol.com>
2016-02-24 13:56:19 -0600

    commits

---
+++ ADGP 130/Assets/Scripts/OutofRange.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutofRange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.transform.tag == "enemy")
+        {
+            hit.transform.SendMessage("", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
